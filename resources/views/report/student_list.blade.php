b83fcdebf48acf2b7a736b41c8ed2ba4fafdbfb3
gabycacho <35979891+gabycacho@users.noreply.github.com>
2018-02-17 10:56:10 -0600

    lista

---
+++ resources/views/report/student_list.blade.php
@@ -0,0 +1,22 @@
+<div class="col-sm-12">
+    <h1>@lang('title.students')</h1>
+</div>
+<div class="col-sm-12">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>@lang('head.name')</th>
+                <th>@lang('head.mobile')</th>
+                <th>@lang('head.phone')</th>
+                <th>@lang('head.emergency_phone')</th>
+                <th>@lang('head.address')</th>
+                <th>@lang('head.email')</th>
+                <th>@lang('head.branch')</th>
+                <th>@lang('head.status')</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+    </table>
+</div>
