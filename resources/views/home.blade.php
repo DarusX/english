082a675acc87a44250273fc449c74354aeac78ed
gabycacho <35979891+gabycacho@users.noreply.github.com>
2018-02-27 13:28:57 -0600

    cambio
    manda error cuando el admin inicia sesion ;(

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,78 +1,11 @@
 @extends('layouts.app')
 @section('side')
 <div class="col-sm-12">
-    @if(Auth::user()->authorizeRoles(['name'=>"Estudiante"]))
-        @include('layouts.menu-student')
-    @else
-        @include('layouts.sidebar')
-    @endif
+@include('layouts.menu')
 </div> 
 @endsection
 @section('content')
 <div class="col-md-10 col-sm-offset-1">
-    <table class="table">
-        <tbody>
-            <tr>
-                <td>@lang('head.name')</td>
-                <td>{{$datos->name}} {{$datos->lastname}}</td>
-            </tr>
-            <tr> 
-                <td>@lang('head.birthdate')</td>
-                <td>{{$datos->birthdate}}</td>
-            </tr>
-            <tr>
-                <td>@lang('head.gender')</td>
-                <td>{{$datos->gender}}</td>
-            </tr>
-            <tr>
-                <td>@lang('head.curp')</td>
-                <td>{{$datos->curp}}</td>
-            </tr>
-            <tr>   
-                <td>@lang('head.mobile')</td>
-                <td>{{$datos->mobile}}</td>
-            </tr>
-            <tr> 
-                <td>@lang('head.phone')</td>
-                <td>{{$datos->phone}}</td>
-            </tr>
-            <tr> 
-                <td>@lang('head.emergency_phone')</td>
-                <td>{{$datos->emergency_phone}}</td>
-            </tr>
-            <tr> 
-                <td>@lang('head.blood_type')</td> 
-                <td>{{$datos->blood_type}}</td>
-            </tr>
-            <tr>
-                <td>@lang('head.allergy')</td>
-                <td>{{$datos->allergy}}</td>
-            </tr>
-            <tr>
-                <td>@lang('head.email')</td>
-                <td>{{$datos->email}}</td>
-            </tr>
-            <tr>
-                <td>@lang('head.address')</td>
-                <td>{{$datos->address}}</td>
-            </tr>
-            <tr> 
-                <td>@lang('head.comments')</td> 
-                <td>{{$datos->comment}}</td>
-            </tr>
-            <tr>
-                <td>@lang('head.registration_date')</td>
-                <td>{{$datos->registration_date}}</td>
-            </tr>
-            <tr>
-                <td>@lang('head.branch')</td>
-                <td>{{$datos->branch->name}}</td>
-            </tr>
-            <tr>
-                <td>@lang('head.status')</td>
-                <td>{{$datos->status->status}}</td>
-            </tr>
-        </tbody>
-    </table>
+    
 </div>
 @endsection
